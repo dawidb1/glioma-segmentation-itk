ef3521d50576ec913ee67adf1d20363e1c8536e8
dawidb1 <dawid.baranski.96@gmail.com>
2020-01-22 16:30:21 +0100

    test bat - automate script

---
+++ test.bat
@@ -0,0 +1,3 @@
+GlioblastomaSegmentation.exe ../../dane/02 ../../wyniki/02a 167;261;14 -ot 177;226;14 -D ../../test_mask/02 -c 1 -cot 0.6 >> logs.txt
+GlioblastomaSegmentation.exe ../../dane/02 ../../wyniki/02 167;261;14 -ot 177;226;14 -D ../../test_mask/02 -c 1 -cot 0.5 >> logs.txt
+GlioblastomaSegmentation.exe ../../dane/02 ../../wyniki/02a 167;261;14 -ot 177;226;14 -D ../../test_mask/02 -c 1 -cot 0.4 >> logs.txt
